1696845aabde01b020a4b45d28695d52c3458e98
Mohit Parashar <mohit1672002@gmail.com>
2021-06-05 12:23:05 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+
+function Header() {
+  const [session] = useSession();
+  const router = useRouter();
+
+  return (
+    <header>
+      {/* top nav bar */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 items-center flex-grow sm:flex-grow-0">
+          <Image
+            onClick={() => router.push("/")}
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow curosr-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        <div className="text-white flex items-center space-x-6 text-xs ml-2 whitespace-">
+          <div
+            onClick={!session ? signIn : signOut}
+            className="cursor-pointer hover:underline"
+          >
+            <p>{session ? `Hello, ${session.user.name}` : "Sign In"}</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="cursor-pointer hover:underline">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative flex items-center cursor-pointer hover:underline"
+          >
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 rounded-full bg-yellow-400 text-center">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm">Basket</p>
+          </div>
+        </div>
+      </div>
+
+      {/* botttom nav bar */}
+      <div className="space-x-3 p-2 pl-6 flex items-center bg-amazon_blue-light text-white">
+        <p className="flex cursor-pointer hover:underline items-center">
+          <MenuIcon className="h-4" />
+          All
+        </p>
+        <p className="cursor-pointer hover:underline ml-2">Prime Video</p>
+        <p className="cursor-pointer hover:underline">Amazon Business</p>
+        <p className="cursor-pointer hover:underline">Today's Deals</p>
+        <p className="cursor-pointer hover:underline hidden lg:inline-flex">
+          Electronics
+        </p>
+        <p className="cursor-pointer hover:underline hidden lg:inline-flex">
+          Food & Grocery
+        </p>
+        <p className="cursor-pointer hover:underline hidden lg:inline-flex">
+          Prime
+        </p>
+        <p className="cursor-pointer hover:underline hidden lg:inline-flex">
+          Shopper Toolkit
+        </p>
+        <p className="cursor-pointer hover:underline hidden lg:inline-flex">
+          Health & Personal Care
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
